4996d30847a25e78907885b744becc89d5fc9056
Tony Ha <noblegoop@gmail.com>
2026-04-19 14:48:14 -0400

    model

--- src/main/java/net/noblegoop/spraypaint/item/ModItems.java
+++ src/main/java/net/noblegoop/spraypaint/item/ModItems.java
@@ -1,17 +1,35 @@
 package net.noblegoop.spraypaint.item;
 
-import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.entity.ItemBasedSteering;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.noblegoop.spraypaint.Spraypaint;
-import org.intellij.lang.annotations.Identifier;
-
-import java.rmi.registry.Registry;
 
 public class ModItems {
+    public static Item register(Item item, String id) {
+        // Create the identifier for the item.
+        ResourceLocation itemID = ResourceLocation.fromNamespaceAndPath(Spraypaint.MOD_ID, id);
 
+        // Register the item.
+        Item registeredItem = Registry.register(BuiltInRegistries.ITEM, itemID, item);
+
+        // Return the registered item!
+        return registeredItem;
+
+    }
+
+    public static final Item SPRAY_BOTTLE = register(
+            new Item(new Item.Properties()),
+            "spray_can"
+    );
+
+    public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.accept(ModItems.SPRAY_BOTTLE));
+    }
 
 
 }
